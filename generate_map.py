910c1c85be6095471aecfc5284b7cc9195baf26a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-02 17:14:56 -0300

    finish: testando aplicação

--- generate_map.py
+++ generate_map.py
@@ -12,10 +12,10 @@ with open('BRAZIL_CITIES.csv', encoding='utf-8', newline='') as csvfile:
     # por cada linha do arquivo
     for row in reader_file:
         # Se a cidade for igual ao item da coluna 0
-        if cidade.lower() == row[0].lower() and uf == row[1]:
+        if cidade.lower() == row[0].lower() and uf.upper() == row[1]:
             print(row[0], row[1], row[23], row[24])
             # Busca a Localização
             map_loc = folium.Map(location=[row[24], row[23]])
             # Cria o arquivo HTML com o mapa
-            map_loc.save(f'{row[0]}_{row[1]}.html')
+            map_loc.save(f'{row[0].replace(" ", "")}_{row[1]}.html')
 
